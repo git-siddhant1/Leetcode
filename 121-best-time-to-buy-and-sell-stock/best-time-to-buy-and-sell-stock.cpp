57a6a7a9276d990b40485d23a4a334faf6d54345
Siddhant Bhalerao <SIDDHANTB.INBOX@GMAIL.COM>
2026-08-06 20:07:15 +0530

    Time: 3 ms (27.80%) | Memory: 97.3 MB (61.34%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int p=0;
         int i=0,j=1;
         for(int j=0;j<prices.size();j++ ){
-            p=max(p,prices[j]-prices[i]);
+            if(prices[j]-prices[i]>p)p=prices[j]-prices[i];
             if(prices[i]>prices[j])i=j;
            
         }
