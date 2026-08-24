244e4d442325546c748fc4babaec34cff6e811c6
Siddhant Bhalerao <SIDDHANTB.INBOX@GMAIL.COM>
2026-08-24 21:33:21 +0530

    Time: 3 ms (49.67%) | Memory: 29 MB (5.23%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        map<int, int> M;
+        for (int num : nums) 
+            M[num]++;
+        for (int num : nums) {
+            if (num % 2 == 0 && M[num] == 1)
+                return num;
+        }
+        return -1;
+    }
+};
