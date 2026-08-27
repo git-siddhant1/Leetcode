2f2e3dc3bedbe85348be0618baf409be492c9b28
Siddhant Bhalerao <SIDDHANTB.INBOX@GMAIL.COM>
2026-08-27 19:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (32.40%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.length()==0)return true;
+        int j=0;
+        for(int i=0;i<t.length();i++){
+            if(j<s.length()){
+                if(s[j]==t[i])j++;}
+        }
+        if(j==s.length()) return true;
+  return false;  }
+};
